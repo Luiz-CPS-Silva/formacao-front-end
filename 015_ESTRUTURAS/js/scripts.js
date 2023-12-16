fc6eabf6040f615794dca257ad4924e4d3029be3
Luiz Carlos <luizx720@gmail.com>
2023-12-16 16:17:08 -0300

    Pasta 015_ESTRUTURAS criada

---
+++ 015_ESTRUTURAS/js/scripts.js
@@ -0,0 +1,42 @@
+// 1 - Variaveis
+let nome = "Luiz";
+
+console.log(nome);
+
+nome = "Luiz Carlos";
+
+console.log(nome);
+
+const idade = 21;
+
+console.log(idade);
+
+console.log(typeof nome);
+
+console.log(typeof idade);
+
+// 2 - mais sobre variaveis
+// let 2teste = "Invalido"
+// let @teste = "Invalido"
+
+let a = 10, b = 20, c = 30;
+
+console.log(a, b, c);
+
+const nomecompleto = "Luiz Silva"
+
+const nomeCompleto = "Batman"
+
+console.log(nomecompleto);
+
+console.log(nomeCompleto);
+
+let _teste = "ok";
+
+let $teste = "ok";
+
+console.log(_teste, $teste);
+
+// 3 - Nomes reservados
+
+// 4 - Ambiente JavaScript
