b7229b070b65c6778192e8979f685dc83b6d7633
Luiz Carlos <luizx720@gmail.com>
2023-12-21 19:09:59 -0300

    Update pasta 0016_FUNCOES

---
+++ 0016_FUNCOES/js/scripts.js
@@ -0,0 +1,21 @@
+// criando uma função
+function minhaFuncao() {
+  console.log("Testando!");
+}
+
+minhaFuncao();
+minhaFuncao();
+
+const minhaFuncaoEmVariavel = function () {
+  console.log("Função em variável");
+};
+
+minhaFuncaoEmVariavel()
+
+function funcaoComParametro(txt) {
+  console.log(`Imprimindo : ${txt}`);
+}
+
+funcaoComParametro("Imprimindo alguma coisa");
+
+funcaoComParametro("Outra função");
